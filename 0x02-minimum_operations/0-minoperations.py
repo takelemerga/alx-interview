90e513b7030df63e357754db5d3e6199479319c3
tolaa <mgamtaa@gmail.com>
2023-01-12 16:20:38 +0300

    minim opr

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+minoperation module
+"""
+
+
+def minOperations(n):
+    """
+    Returns the fewest number of operations needed
+    to result in exactly n H characters in the file
+    """
+    oper = 0
+    min_oper = 2
+    while n > 1:
+        while n % min_oper == 0:
+            oper += min_oper
+            n /= min_oper
+        min_oper += 1
+    return oper
