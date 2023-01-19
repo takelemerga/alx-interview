1469b706815e73842c00c499a9675ba373f3db9a
tolaa <mgamtaa@gmail.com>
2023-01-19 17:31:14 +0300

    fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -22,21 +22,16 @@ status_code_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
 file_size_total = 0
 number_of_lines = 0
 
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            try:
-                line = line.replace("\n", "")
-                status_code = line.split()[-2]
-                if status_code in status_code_dict.keys():
-                    status_code_dict[status_code] += 1
-                file_size = int(line.split()[-1])
-                file_size_total += file_size
-            except Exception:
-                pass
-            number_of_lines += 1
-            if number_of_lines % 10 == 0 and number_of_lines > 0:
-                print_statistics(file_size_total, status_code_dict)
-    except KeyboardInterrupt:
-        print_statistics(file_size_total, status_code_dict)
-        raise
+try:
+    for line in sys.stdin:
+        line = line.replace("\n", "")
+        status_code = line.split()[-2]
+        if status_code in status_code_dict.keys():
+            status_code_dict[status_code] += 1
+        file_size = int(line.split()[-1])
+        file_size_total += file_size
+        number_of_lines += 1
+        if number_of_lines % 10 == 0 and number_of_lines > 0:
+            print_statistics(file_size_total, status_code_dict)
+finally:
+    print_statistics(file_size_total, status_code_dict)
