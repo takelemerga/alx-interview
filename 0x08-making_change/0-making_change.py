778957468520f191d90916fd952cb203aa760c4d
tolaa <mgamtaa@gmail.com>
2023-02-23 11:40:43 +0300

    make change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""pile module"""
+
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins needed to meet a given amount total
+    given a pile of coins of different values
+    """
+
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+    temp = 0
+
+    for coin in coins:
+        if total % coin <= total:
+            temp += total // coin
+            total = total % coin
+    return temp if total == 0 else -1
