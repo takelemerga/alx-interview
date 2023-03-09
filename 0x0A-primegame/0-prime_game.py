6a3b2fd1db44937664109004ffc5dde94e20f50d
tolaa <mgamtaa@gmail.com>
2023-03-09 17:24:30 +0300

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+"""prime game module"""
+
+mariaWins = 0
+benWins = 0
+winner = ""
+
+
+def isWinner(x, nums):
+    """returns the winner"""
+    global mariaWins
+    global benWins
+
+    for i in nums:
+        playgame(i)
+    if (mariaWins > benWins):
+        winner = "MARIA"
+    elif (mariaWins < benWins):
+        winner = "BEN"
+    else:
+        winner = "None"
+    return winner
+
+
+def playgame(num_integers):
+    """play game"""
+
+    lst = []
+    for j in range(1, num_integers + 1):
+        lst.append(j)
+
+    mariaPlay(lst)
+
+
+def mariaPlay(lst):
+    """maria play"""
+    prime = False
+    for k in lst:
+        prime = isPrime(k)
+        if (prime):
+            for h in range(k):
+                multiples = h * k
+                if (multiples in lst):
+                    lst.remove(multiples)
+    if (prime):
+        benPlay(lst)
+    else:
+        global benWins
+        benWins += 1
+
+
+def benPlay(lst):
+    """ben play"""
+    prime = False
+    for k in lst:
+        prime = isPrime(k)
+        if (prime):
+            for h in range(k):
+                multiples = h * k
+                if (multiples in lst):
+                    lst.remove(multiples)
+    if (prime):
+        mariaPlay(lst)
+    else:
+        global mariaWins
+        mariaWins += 1
+
+
+def isPrime(n):
+    """check number"""
+    if (n <= 1):
+        return False
+    for i in range(2, n):
+        if (n % i == 0):
+            return False
+    return True
